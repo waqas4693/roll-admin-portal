7898962f03e22cb012fd9bbd892133554fbef822
Waqas Ahmed <waqas4693@gmail.com>
2024-05-10 00:43:11 +0500

    Url Error Fix

--- client/src/config/server-url.jsx
+++ client/src/config/server-url.jsx
@@ -1,3 +1,3 @@
-const url = 'https://https://roll-admin-portal-sgqk.vercel.app/'
+const url = 'https://roll-admin-portal-sgqk.vercel.app/'
 //  const url = 'http://localhost:5001/'
 export default url
