44bcf047cde545e9c8fa692656c3388925abf847
Waqas Ahmed <waqas4693@gmail.com>
2024-05-10 16:17:25 +0500

    Error Fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import helmet from 'helmet'
 import morgan from 'morgan'
@@ -30,47 +31,51 @@ app.use('/api/auth', authRoutes)
 app.use('/api/roll', rollRoutes)
 app.use('/api/adminLogin', adminLogin)
 
-
-import { MongoClient, ServerApiVersion } from 'mongodb';
-const uri = "mongodb+srv://waqas:waqas123456@cluster0.9qqkzja.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// import { MongoClient, ServerApiVersion } from 'mongodb'
+// const uri =
+//   'mongodb+srv://waqas:waqas123456@cluster0.9qqkzja.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true
+//   }
+// })
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("test").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
+// async function run () {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect()
+//     // Send a ping to confirm a successful connection
+//     await client.db('test').command({ ping: 1 })
+//     console.log(
+//       'Pinged your deployment. You successfully connected to MongoDB!'
+//     )
+//   } catch (e) {
+//     console.log('Mongo Error = ')
+//     console.log(e)
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close()
+//   }
+// }
+// run().catch(console.dir)
 
 /* MONGOOSE SETUP */
-// const PORT = process.env.PORT || 9000
-// mongoose.set('strictQuery', false)
+const PORT = process.env.PORT || 9000
+mongoose.set('strictQuery', false)
 
-// mongoose
-// .connect('mongodb://waqas:waqas123456@cluster0.9qqkzja.mongodb.net/test', {
-//   // .connect(process.env.MONGO_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   })
-//   .then(() => {
-//     app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
-//   })
-//   .catch(error => console.log(`${error} did not connect`))
+mongoose
+.connect('mongodb://waqas:waqas123456@cluster0.9qqkzja.mongodb.net/test', {
+  // .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+  })
+  .catch(error => console.log(`${error} did not connect`))
 
 export default app
